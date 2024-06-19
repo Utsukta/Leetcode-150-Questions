b7acf5d18f3a4f8c7056c14fe317fe72d46cb6cd
Utsukta Khatri <utsukta99kc@gmail.com>
2024-06-19 12:23:03 +0545

    Majority Element-Boyer-Moore Voting Algorithm

---
+++ Array_and_String/Majority_Element_169.py
@@ -0,0 +1,50 @@
+#169. Majority Element
+
+#Inefficient one
+# import math as m
+# class Solution(object):
+#     def majorityElement(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         length=m.floor(len(nums)/2)
+#         print(length)
+#         i=0
+#         nums=sorted(nums)
+#         while i<len(nums)-1:
+#             if nums[i]!=nums[i+1]:
+#                 count=i+1
+#                 i+=1
+#                 print(count)
+#                 if count>length:
+#                     element=nums[i-1] 
+#                     return element  
+#             else:
+#                 element=nums[i]
+#                 i+=1
+#         return element
+
+
+
+#Boyer-Moore Voting Algorithm to find the majority algorithm
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+        
+        return candidate
+      
+
+s=Solution()
+output=s.majorityElement([6,6,6,7,7,])
+print(output)
