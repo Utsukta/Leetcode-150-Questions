ca40586551198a4be7232cee6a9a949bf1213a92
Utsukta Khatri <utsukta99kc@gmail.com>
2024-06-19 10:00:07 +0545

    Remove Duplicates from Sorted Array II

--- Array_and_String/Remove_Duplicate_II_80.py
+++ Array_and_String/Remove_Duplicate_II_80.py
@@ -1,3 +1,7 @@
+#80. Remove Duplicates from Sorted Array II
+#Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice.
+# The relative order of the elements should be kept the same.
+
 class Solution(object):
     def removeDuplicates(self, nums):
         """
